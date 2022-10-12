9e32efa83762c28d8c3694b1049c4b8b9605cdef
michael-bocim <mtbocim@gmail.com>
2022-10-11 19:10:29 -0600

    completed special word finder

--- wordfinder.py
+++ wordfinder.py
@@ -6,30 +6,40 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, path):
-        """Initialize variables"""
+        """Initialize instance with a given path and populate words list"""
+        print("WordFinder __init__")
         self.path = path
-        self.words = []
-        self.get_words()
+        self.words = self.get_words()
         print (f"{len(self.words)} words read")
 
     def __repr__(self):
-        """Display word count"""
+        """Display word count of instance"""
+        print("WordFinder __repr__")
         return f"There are {len(self.words)} words"
 
     def random(self):
-        """Pick random word"""
+        """Pick random word from list of words"""
+        print("WordFinder random")
         return random.choice(self.words)
 
     def get_words(self):
         """Parse file and populates words list"""
+        print("WordFinder get_words")
+        words = []
         with open(self.path) as file:
             for line in file:
-                self.words.append(line.rstrip())
+                words.append(line.rstrip())
         file.close()
- """Filter for blank lines and #"""
-class SpecialWordFinder(WordFinder):
-    """Init"""
-    def __init__ (self,path):
-        super().__init__(path)
+        return words
 
-    def 
+"""Filter for blank lines and #"""
+class SpecialWordFinder(WordFinder):
+    """Initialize instance with a given path and populate words list"""
+    
+    #only need __init__ if new params
+
+    def get_words(self):
+        """Parse file and populates words list"""
+        print("SpecialWordFinder get_words")
+        words = super().get_words()
+        return [word for word in words if len(word)!=0 and word[0]!='#']
