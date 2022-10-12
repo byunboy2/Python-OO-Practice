1042c3339b6e5c3fa735c533aa263d478cd9e82b
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-11 17:04:36 -0700

    Finished first half of wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,35 @@
+import random
+
+
 class WordFinder:
+
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Initialize variables"""
+        self.path = path
+        self.words = []
+        self.get_words()
+        print (f"{len(self.words)} words read")
+
+    def __repr__(self):
+        """Display word count"""
+        return f"There are {len(self.words)} words"
+
+    def random(self):
+        """Pick random word"""
+        return random.choice(self.words)
+
+    def get_words(self):
+        """Parse file and populates words list"""
+        with open(self.path) as file:
+            for line in file:
+                self.words.append(line.rstrip())
+        file.close()
+ """Filter for blank lines and #"""
+class SpecialWordFinder(WordFinder):
+    """Init"""
+    def __init__ (self,path):
+        super().__init__(path)
+
+    def 
